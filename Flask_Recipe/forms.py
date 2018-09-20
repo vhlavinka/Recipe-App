b10e3122e88005c39a04947fe6c4a09b403207ac
Valerie <hlavinkav@yahoo.com>
2018-09-19 21:30:17 -0400

    Add files via upload

--- Flask_Recipe/forms.py
+++ Flask_Recipe/forms.py
@@ -5,7 +5,7 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=5,max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=5,max=20)])
     reenter = PasswordField('Re-enter password', validators=[DataRequired(), EqualTo('password')])
 
     submit = SubmitField('Sign up')
@@ -16,3 +16,8 @@ class LoginForm(FlaskForm):
     session = BooleanField('Remember me')
 
     submit = SubmitField('Sign in')
+
+class EnterRecipe(FlaskForm):
+    recipe_url = StringField('Recipe URL', validators=[DataRequired()])
+
+    submit = SubmitField('Get Ingredients')
